a050667f6717392bb6df905cfedbc0b80bf38206
titoycute <titoycustodio@gmail.com>
2026-02-16 14:46:50 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'salig-v1';
+const ASSETS = [
+  'index.html',
+  'login.html',
+  'manager.html',
+  'public_view.html',
+  'logo.png',
+  'config.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+
+
+// sw.js
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
